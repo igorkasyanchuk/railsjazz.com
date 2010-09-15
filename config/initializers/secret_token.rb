91236bb3a90bd2214e3aefc78a6d0a3c01c4a937
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2010-09-15 16:44:10 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsjazzCom::Application.config.secret_token = 'a8b7c644fe151850d52ae7c2eb374c0dcb973b3c9cbe715c5ea8ad87002290976a77d2845137daf7f3e81945b1e2ba4772f134c82ab1884ff8599e702066be83'
