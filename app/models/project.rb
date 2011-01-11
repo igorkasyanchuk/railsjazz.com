a43115fa82b7dd304e111ae3733dccb81d53b0e2
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2011-01-11 08:23:15 -0500

    new changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,7 +13,7 @@ class Project < ActiveRecord::Base
   scope :featured, where(:featured => true)
   scope :web_design, where(:project_category_id => ProjectCategory.find_by_name("Web Design").id)
   scope :web_development, where(:project_category_id => ProjectCategory.find_by_name("Web Development").id)
-  scope :latest, order('completed_on desc')
+  scope :latest, order('completed_on desc').limit(5)
 
   has_attached_file :image, 
                     :storage => :s3,
